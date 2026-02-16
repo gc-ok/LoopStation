f23890bfc742b3b826c703fc4c6f08b9a51485fd
gc-ok <gerardo.cast23@gmail.com>
2026-02-15 23:39:09 -0600

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,5 +3,6 @@ Utility functions for Loop Station.
 """
 
 from .formatting import format_time, parse_time
+from .tooltip import ToolTip
 
-__all__ = ['format_time', 'parse_time']
+__all__ = ['format_time', 'parse_time', 'ToolTip']
